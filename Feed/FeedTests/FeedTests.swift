748b26628ff643d0164d4d4d9b38855ad17137dd
Rafael Rios <rafaelrafssk@hotmail.com>
2025-12-27 16:39:49 -0300

    Guarantee we only receive one error

--- Feed/FeedTests/FeedTests.swift
+++ Feed/FeedTests/FeedTests.swift
@@ -60,12 +60,14 @@ struct RemoteFeedLoaderTests {
     
     @Test func load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
         
-        var capturedError: RemoteFeedLoader.Error?
-        sut.load { error in capturedError = error }
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
     
-        #expect(capturedError == .connectivity)
+        #expect(capturedErrors == [.connectivity])
     }
     
     // MARK: - Helpers
@@ -79,13 +81,10 @@ struct RemoteFeedLoaderTests {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
-            
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }

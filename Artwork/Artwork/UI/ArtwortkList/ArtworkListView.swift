c338fcb1303da6bd47b7520d893adc87434c86eb
Rafael Rios <rafaelrafssk@hotmail.com>
2026-01-10 16:37:42 -0300

    Move ArtworkListViewModel to its own file

--- Artwork/Artwork/UI/ArtwortkList/ArtworkListView.swift
+++ Artwork/Artwork/UI/ArtwortkList/ArtworkListView.swift
@@ -6,32 +6,6 @@
 //
 
 import SwiftUI
-import Combine
-
-public enum ImageResult {
-    case failure
-    case success(Image)
-}
-
-public struct ArtworkListViewData {
-    let list: [(model: ArtworkCardViewModel, imageResult: ImageResult)]
-    
-    public init(list: [(model: ArtworkCardViewModel, imageResult: ImageResult)]) {
-        self.list = list
-    }
-}
-
-public protocol ArtworkListViewModelProtocol {
-    var data: ArtworkListViewData { get set }
-}
-
-class ArtworkListViewModel: ArtworkListViewModelProtocol {
-    var data: ArtworkListViewData
-    
-    init(data: ArtworkListViewData = ArtworkListViewData(list: [])) {
-        self.data = data
-    }
-}
 
 public struct ArtworkListView<ViewModel: ArtworkListViewModelProtocol>: View {
     private var viewModel: ViewModel

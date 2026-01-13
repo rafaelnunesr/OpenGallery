b5619b5dac10bc3fc0332a3ad30adea9ab7453ff
Rafael Rios <rafaelrafssk@hotmail.com>
2026-01-13 15:18:11 -0300

    Move AsyncImagePhaseView to its own file

--- Artwork/Artwork/UI/ImageView/ImageView.swift
+++ Artwork/Artwork/UI/ImageView/ImageView.swift
@@ -7,56 +7,6 @@
 
 import SwiftUI
 
-public struct AsyncImagePhaseView: View {
-    private let phase: AsyncImagePhase
-    private let retryAction: () -> Void
-    
-    public init(phase: AsyncImagePhase, retryAction: @escaping () -> Void) {
-        self.phase = phase
-        self.retryAction = retryAction
-    }
-    
-    public var body: some View {
-        switch phase {
-        case .empty:
-            ProgressView()
-        case let .success(image):
-            image
-                .resizable()
-                .scaledToFit()
-        case .failure:
-            retryView
-        @unknown default:
-            ProgressView()
-        }
-    }
-    
-    private var retryView: some View {
-        Button {
-            retryAction()
-        } label: {
-            ZStack {
-                Color.red.opacity(0.3)
-                
-                VStack(alignment: .center, spacing: 8) {
-                    Image(systemName: "arrow.trianglehead.clockwise")
-                        .font(.title)
-                        .foregroundStyle(Color.white)
-                    
-                    Text("Retry")
-                        .font(.title)
-                        .foregroundStyle(Color.white)
-                }
-            }
-        }
-        .accessibilityIdentifier(AccessibilityIds.button)
-    }
-    
-    public enum AccessibilityIds {
-        public static let button = "retry_button"
-    }
-}
-
 public struct ImageView: View {
     private let url: URL
     @State private var imageId = UUID()

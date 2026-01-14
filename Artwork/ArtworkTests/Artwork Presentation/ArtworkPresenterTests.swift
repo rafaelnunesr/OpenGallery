fd4cb9900e29a5bb0ed325a03619a78f7d82c042
Rafael Rios <rafaelrafssk@hotmail.com>
2026-01-14 17:11:02 -0300

    Verify didStartLoading cleans error message and sets isLoading as true

--- Artwork/ArtworkTests/Artwork Presentation/ArtworkPresenterTests.swift
+++ Artwork/ArtworkTests/Artwork Presentation/ArtworkPresenterTests.swift
@@ -8,22 +8,38 @@
 import Testing
 import SwiftUI
 import Artwork
-internal import Combine
+import Combine
 
-protocol ResourceView: ObservableObject {
-    associatedtype ViewState
-    var state: ViewState { get }
+protocol ArtworkListViewState: ObservableObject {
+    var list: [ArtworkCardViewModel] { get set }
 }
 
-struct ArtworkListViewState {
-    let list: [ArtworkCardViewModel]
+protocol LoadingViewState: ObservableObject {
+    var isLoading: Bool { get set }
+}
+
+protocol ErrorViewState: ObservableObject {
+    var errorMessage: String? { get set }
 }
 
 class ArtworkPresenter {
-    private let resourceView: any ResourceView
+    private let resourceViewState: any ArtworkListViewState
+    private let loadingViewState: any LoadingViewState
+    private let errorViewState: any ErrorViewState
     
-    init(resourceView: any ResourceView) {
-        self.resourceView = resourceView
+    init(
+        resourceViewState: any ArtworkListViewState,
+        loadingViewState: any LoadingViewState,
+        errorViewState: any ErrorViewState,
+    ) {
+        self.resourceViewState = resourceViewState
+        self.loadingViewState = loadingViewState
+        self.errorViewState = errorViewState
+    }
+    
+    func didStartLoading() {
+        loadingViewState.isLoading = true
+        errorViewState.errorMessage = nil
     }
 }
 
@@ -31,20 +47,63 @@ struct ArtworkPresenterTests {
     
     @Test
     func init_doesNotSendMessagesToView() {
-        let resourceView = ResourceViewSpy()
-        _ = ArtworkPresenter(resourceView: resourceView)
+        let viewState = makeSUT().viewState
         
-        #expect(resourceView.messages.isEmpty)
+        #expect(viewState.messages.isEmpty)
+    }
+    
+    @Test
+    func didStartLoading_displaysNoErrorMessageAndStartsLoading() {
+        let (sut, viewState) = makeSUT()
+        
+        sut.didStartLoading()
+        
+        #expect(viewState.messages == [.loading(true), .error(nil)])
     }
     
     // MARK: - Helpers
     
-    private class ResourceViewSpy: ResourceView {
-        @Published var state: ArtworkListViewState
-        var messages = [ArtworkListViewState]()
+    private typealias SUT = (sut: ArtworkPresenter, viewState: ViewStateSpy)
+    
+    private func makeSUT() -> SUT {
+        let viewState = ViewStateSpy()
         
-        init(state: ArtworkListViewState = .init(list: [])) {
-            self.state = state
+        let sut = ArtworkPresenter(
+            resourceViewState: viewState,
+            loadingViewState: viewState,
+            errorViewState: viewState
+        )
+        
+        return (sut, viewState)
+    }
+    
+    private class ViewStateSpy: ArtworkListViewState, LoadingViewState, ErrorViewState {
+        @Published var list = [ArtworkCardViewModel]() {
+            didSet {
+                messages.insert(.list(list))
+            }
         }
+        
+        @Published var isLoading: Bool = false {
+            didSet {
+                messages.insert(.loading(isLoading))
+            }
+        }
+        
+        @Published var errorMessage: String? = nil {
+            didSet {
+                messages.insert(.error(errorMessage))
+            }
+        }
+        
+        enum Message: Hashable {
+            case list([ArtworkCardViewModel])
+            case loading(Bool)
+            case error(String?)
+        }
+        
+        private(set) var messages = Set<Message>()
+        
+        init() {}
     }
 }

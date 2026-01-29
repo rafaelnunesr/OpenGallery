ac6c914ff0aa7026a4dc9c114136e7d4ed055a42
Rafael Rios <rafaelrafssk@hotmail.com>
2026-01-29 17:39:34 -0300

    Create empty ios app

---
+++ OpenGalleryApp/OpenGalleryApp/OpenGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenGalleryAppApp.swift
+//  OpenGalleryApp
+//
+//  Created by Rafael Rios on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
